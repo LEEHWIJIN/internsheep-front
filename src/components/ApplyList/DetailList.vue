064cda229c7e0d1c1f95c45444bb292b170525e2
leehwijin <cysoon96@gmail.com>
2019-05-02 14:55:12 +0900

    Implement company applylist & apply company

---
+++ src/components/ApplyList/DetailList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="applyList">
+    <div class="comanyList">
+        <div v-for='sc in selectedCo' :key="sc.cNoticeID">
+          기업명 : {{sc.cName}}<br>
+          매니저 : {{sc.cManagerName}}<br><br>
+          <button @click="applyStd(sc.cName)">지원하기</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default{
+      name: 'applyList',
+      data() {
+        return {
+            sName:"이모씨"
+        }
+      },
+      components: {
+        
+      },
+      props:{
+        selectedCo:{
+            type:Array,
+            required: true,
+        },
+      },
+      created(){
+
+    },
+      methods: {
+        applyStd(cName){
+            //지원하기 전에 이 학생이 지원했는지에 대한 여부를 판단하는 것(get)도 필요함.
+            this.$http.post('http://localhost:8888/std/mypage/applyCo',{cName : cName, sName : this.sName}).then((response) => {
+              //팝업을 띄우던 그런식으로 해야 할 것 같음.
+            })
+        },
+      }
+  }
+</script>
+
+<style scoped>
+#applyList{
+  margin-left: 600px;
+  margin-top: 600px;
+}
+</style>
--- src/components/ApplyList/Index.vue
+++ src/components/ApplyList/Index.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="apply">
+    <!-- <v-CoList :applylist="applylist" @clickCo="clickCo"></v-CoList> -->
     <div class="comanyList">
-        <div v-for='(AL,index) in applylist' :key="AL.cNoticeID">
+        <div v-for='(AL,index) in applylist' :key="AL.cNoticeID" @click="clickCo(index)">
           {{index+1}}.
           기업명 : {{AL.cName}}<br>
           매니저 : {{AL.cManagerName}}<br><br>
         </div>
     </div>
+    <v-detail-list :selectedCo="selectedCo"></v-detail-list>
   </div>
 </template>
 
 
 <script>
+// import VCoList from "./CoList.vue"
+ import VDetailList from "./DetailList.vue"
   export default{
       name: 'Apply',
       data() {
         return {
-            applylist:[],
+          applylist:[],
+          selectedCo:[],
         }
       },
       components: {
-          
+          //VCoList,
+          VDetailList,
       },
       created(){
         this.applyList();
+      
       },
       methods: {
         applyList(){
@@ -37,12 +44,21 @@
               }
             })
         },
+        clickCo(selectedNum){
+          this.selectedCo=[]
+          console.log(selectedNum)
+          this.selectedCo.push({
+            cManagerName : this.applylist[selectedNum].cManagerName,
+            cName : this.applylist[selectedNum].cName,
+            cNoticeID : this.applylist[selectedNum].cNoticeID
+          })
+        },
       }
   }
 </script>
 
 <style scoped>
-#resume{
+#apply{
   margin-left: 200px;
   margin-top: 200px;
 }
