e89ba98757ed34c26d1f20c6150cc42d00185f24
leehwijin <cysoon96@gmail.com>
2019-05-07 15:39:39 +0900

    Add passed?

--- src/components/ApplyStatus/Index.vue
+++ src/components/ApplyStatus/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="applystatus">
     내 지원현황<br><br>
-     회사명 : {{cName}}
+     회사명 : {{cName}}<br>
+     합격여부 : {{YN}}
   </div>
 </template>
 
@@ -12,6 +13,7 @@
       return {
         sName : "이모씨",
         cName : "",
+        YN : "",
       }
     },
     components: {
@@ -24,6 +26,8 @@
       getApplyStatus() {
         this.$http.get('http://localhost:8888/std/mypage/applyStatus',{params:{sName : this.sName}}).then((response)=>{
           this.cName = response.data[0].cName;  
+          if(response.data[0].YN) this.YN = "합격입니다.";
+          else this.YN = "심사중입니다."
         })
       }
     }
