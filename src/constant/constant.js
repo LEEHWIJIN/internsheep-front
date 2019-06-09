0dc8e6992292e1ecfc5d3cd90637d448545784bb
leehwijin <cysoon96@gmail.com>
2019-06-10 00:43:13 +0900

    ?

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,11 +1,5 @@
 const API_SERVER = "http://localhost:8888"
-<<<<<<< HEAD
 // const API_SERVER = "http://api.ajou-internsheep.co"
-=======
- // const API_SERVER = "http://api.ajou-internsheep.co"
-
-
->>>>>>> 7b04df2beb7cc70ee8c57901919beb7f3464a9e7
 export default {
     API_SERVER: API_SERVER
 }
