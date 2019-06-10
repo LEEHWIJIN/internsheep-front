d212ccdb7d1db9233eb853257e613c69e20ee308
chj777 <chj777@ajou.ac.kr>
2019-06-10 16:51:47 +0900

    profile footer

--- src/components/Profile/Index.vue
+++ src/components/Profile/Index.vue
@@ -1,21 +1,24 @@
 <!-- 회원 정보 수정 페이지  -->
 <template>
-<section class="section section-lg-bottom bg-light">
-  <div  class="container" id="apply">
-    <v-base></v-base>
-    <div class="row">
-    <!-- 카테고리란 -->
-    <div class="col-lg-3">
-      <v-category></v-category>
-    </div>
-    <!-- 공백 -->
-    <div class="col-lg-1">
-    </div>
-    <v-read-profile v-if="$store.state.profile.profile_state==0"></v-read-profile>
-    <v-modify-profile v-if="$store.state.profile.profile_state==1"></v-modify-profile>
+  <div class="">
+    <section class="section section-lg-bottom bg-light">
+      <div  class="container" id="apply">
+        <v-base></v-base>
+        <div class="row">
+        <!-- 카테고리란 -->
+        <div class="col-lg-3">
+          <v-category></v-category>
+        </div>
+        <!-- 공백 -->
+        <div class="col-lg-1">
+        </div>
+        <v-read-profile v-if="$store.state.profile.profile_state==0"></v-read-profile>
+        <v-modify-profile v-if="$store.state.profile.profile_state==1"></v-modify-profile>
+      </div>
+      </div>
+    </section>
+    <v-footer></v-footer>
   </div>
-  </div>
-</section>
 </template>
 
 <script>
@@ -23,6 +26,7 @@
   import VCategory from '../Category/Index.vue'
   import VReadProfile from './ReadProfile.vue'
   import VModifyProfile from './ModifyProfile.vue'
+  import VFooter from '../Footer/Index.vue'
   import Const from '../../constant/constant';
   export default{
       name: 'Apply',
@@ -40,6 +44,7 @@
         VCategory,
         VReadProfile,
         VModifyProfile,
+        VFooter,
       },
       async created(){
         await this.$store.dispatch('profile/setProfileState',0);
